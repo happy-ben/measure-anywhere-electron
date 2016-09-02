909f75c8675ff04e853250b76fe1c03647c78a2c
Benjamin Doerig <benjamin.doerig@zuhlke.com>
2016-09-02 11:45:40 +0100

    feat(push-to-web)

---
+++ push-to-web.ino
@@ -0,0 +1,32 @@
+int boardLed = D7;
+int photoresistor = A0;
+int power = A5;
+
+int initialValue;
+
+void setup() {
+    pinMode(boardLed,OUTPUT);
+    digitalWrite(boardLed, LOW);
+    
+    pinMode(power,OUTPUT);
+    digitalWrite(power,HIGH);
+    
+    pinMode(photoresistor,INPUT);
+    
+    initialValue = analogRead(photoresistor);
+}
+
+void loop() {
+    int currentValue = analogRead(photoresistor);
+    if((initialValue - currentValue) > 300){
+        Particle.publish("lightStatus","darker",60,PRIVATE);
+        blink();
+    }
+    delay(100);
+}
+
+void blink(){
+    digitalWrite(boardLed,HIGH);
+    delay(500);
+    digitalWrite(boardLed,LOW);
+}
